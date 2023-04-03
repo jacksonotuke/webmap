abb5acb7fb8683ec5cef2226d66190b111bd9d3f
jacksonotuke <129754100+jacksonotuke@users.noreply.github.com>
2023-04-03 22:39:15 +0300

    Add files via upload

---
+++ data/CollectionCenters_12.js
@@ -0,0 +1,10 @@
+var json_CollectionCenters_12 = {
+"type": "FeatureCollection",
+"name": "CollectionCenters_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Field1": "Existing Collection Center" }, "geometry": { "type": "Point", "coordinates": [ 36.7940258, -1.3121977 ] } },
+{ "type": "Feature", "properties": { "Field1": "Proposed Collection Center" }, "geometry": { "type": "Point", "coordinates": [ 36.792389, -1.315694 ] } },
+{ "type": "Feature", "properties": { "Field1": "Proposed Collection Center" }, "geometry": { "type": "Point", "coordinates": [ 36.7864486, -1.3115424 ] } }
+]
+}
---
+++ data/ConsolidationCenters_11.js
@@ -0,0 +1,9 @@
+var json_ConsolidationCenters_11 = {
+"type": "FeatureCollection",
+"name": "ConsolidationCenters_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Field1": "Existing Consolidation center" }, "geometry": { "type": "Point", "coordinates": [ 36.8619606, -1.3787522 ] } },
+{ "type": "Feature", "properties": { "Field1": "Proposed Consolidation Center" }, "geometry": { "type": "Point", "coordinates": [ 36.796332, -1.313936 ] } }
+]
+}
